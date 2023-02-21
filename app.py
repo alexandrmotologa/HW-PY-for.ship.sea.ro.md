85f11e8295446376ec6a659fc5c96c4d1e3ac04f
Alexander Minaku <al.motologa@gmail.com>
2023-02-21 10:22:04 +0200

    HW

---
+++ app.py
@@ -0,0 +1,8 @@
+big_ship = int(input("Introduceți coordonata vaporului mare: "))
+for x in range(1, 11):
+    if x == big_ship:
+        print("W", end="")
+    elif x == big_ship - 1 or x == big_ship + 1:
+        print("w", end="")
+    else:
+        print("~", end="")
